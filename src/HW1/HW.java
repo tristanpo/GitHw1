98e85e6d6aca23d5fe7e996ef92765b45598ccb5
tristanpo <poseleytd@gmail.com>
2022-05-15 20:35:36 -0400

    "2nd Commit"

--- src/HW1/HW.java
+++ src/HW1/HW.java
@@ -2,7 +2,7 @@ package HW1;
 
 public class HW {
     public static void main(String[] args) {
-        for(int i=1; i<=100; i++){
+        for(int i=1; i<=50; i++){
             System.out.println(i);
         }
     }
